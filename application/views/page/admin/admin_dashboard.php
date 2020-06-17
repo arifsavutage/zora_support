9db6d34c9b57efb19d0524f98125cc0adabb42f4
arifsavutage <arifsavutage@gmail.com>
2020-06-17 14:16:09 +0700

    update disable view  pie chart

--- application/views/page/admin/admin_dashboard.php
+++ application/views/page/admin/admin_dashboard.php
@@ -198,7 +198,7 @@
 <div class="row">
 
     <!-- Area Chart -->
-    <div class="col-xl-8 col-lg-7">
+    <div class="col-md-12 col-xl-12 col-lg-12">
         <div class="card shadow mb-4">
             <!-- Card Header - Dropdown -->
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -213,10 +213,10 @@
         </div>
     </div>
 
-    <!-- Pie Chart -->
+    <!-- Pie Chart 
     <div class="col-xl-4 col-lg-5">
         <div class="card shadow mb-4">
-            <!-- Card Header - Dropdown -->
+            
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
                 <div class="dropdown no-arrow">
@@ -232,7 +232,7 @@
                     </div>
                 </div>
             </div>
-            <!-- Card Body -->
+            
             <div class="card-body">
                 <div class="chart-pie pt-4 pb-2">
                     <canvas id="myPieChart"></canvas>
@@ -250,5 +250,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>-->
 </div>
